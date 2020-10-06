ea328a44690dd5038c7ca00a04adb151f3556a2a
Luiz Grooters Júnior <61830954+grootersluiz@users.noreply.github.com>
2020-10-06 19:38:42 -0300

    Create Person.java

---
+++ 13/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    int age;
+public static void main(String[] args) {
+    Person myDude = new Person();
+    myDude.name = "Luiz";
+    myDude.age = 26;
+    System.out.println("His name is: " + myDude.name + "\n" + "His age is: " + myDude.age);    
+    
+ }
+}
